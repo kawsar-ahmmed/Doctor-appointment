7b01c0c843d1c6d97745552a3678a2516b540aca
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-06-18 06:30:42 +0600

    Initial Setup

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className=' container mx-auto m-auto '>
+            <h1 className=' font-bold text-4xl'>This is home page</h1>
+        </div>
+    );
+};
+
+export default Home;
